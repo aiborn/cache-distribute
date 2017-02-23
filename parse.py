8438f627aff1c8c066f7cddc3f094c27868e4363
itsankoff <ivaylotsankov93@gmail.com>
2017-02-23 21:59:20 +0200

    * Add caches and videos in ouptut

--- parse.py
+++ parse.py
@@ -15,11 +15,18 @@ def parse(file_path):
     with open(file_path) as file:
         line_args = file.readline().split(' ')
         args = [int(x) for x in line_args]
-        [videos, endpoints_cnt, requests, caches, cache_size] = args
+        [video_cnt, endpoints_cnt, requests, caches_cnt, cache_size] = args
 
+        # parse endpoints
+        caches = {}
+        for cid in range(caches_cnt):
+            caches[cid] = cache_size
+
+        # parse videos sizes
         line_args = file.readline().split(' ')
-        videos_size = [int(x) for x in line_args]
+        videos = [int(x) for x in line_args]
 
+        # parse endpoints
         endpoints = {}
         for endpoint_id in range(endpoints_cnt):
             line = file.readline()
@@ -31,7 +38,6 @@ def parse(file_path):
             [dc_latency, cache_servers] = [int(x) for x in line.split(' ')]
             endpoint = {
                 'datacenter_latency': dc_latency,
-                'cache_size': cache_size,
                 'cache': {},
                 'requests': {},
             }
@@ -46,17 +52,20 @@ def parse(file_path):
             [video_id, endpoint_id, req_cnt] = [int(x) for x in line.split(' ')]
             endpoints[endpoint_id]['requests'][video_id] = req_cnt
 
-        return endpoints
+        return [endpoints, caches, videos]
 
 def main():
     if len(sys.argv) < 2:
         print('Need input file')
         exit(1)
 
-    endpoints = parse(sys.argv[1])
+    [endpoints, caches, videos_size] = parse(sys.argv[1])
     for eid, e in endpoints.items():
         print(eid, e)
+    print(caches)
+    print(videos_size)
 
 
 if __name__ == '__main__':
+    print(parse(sys.argv[1]))
     main()
