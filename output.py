404877d544a4dd19f90daf149d92967ef488a92b
IchoKatsarski <iliankatsarski@gmail.com>
2017-02-23 21:36:47 +0200

    Output func now uses dict as an input

--- output.py
+++ output.py
@@ -1,17 +1,20 @@
 import sys
 
-# list of tuples
-# first elem of tuple is cache server, second is list of videos
-entities_example = [(0,[2]),
-            (1,[3,1]),
-            (2,[0,1])]
+# map of cache servers and list of videos
+
+# entities_example = {
+#     0: [2],
+#     1: [3,1],
+#     2: [0,1]
+# }
+
 ''' Prints data according to submission file format '''
 def generate_output(number_of_servers,entities):
     print(number_of_servers)
-    for item in entities:
-        string=str(item[0])
+    for k,v in entities.items():
+        string=str(k)
         string+=' '
-        for i in item[1]:
+        for i in v:
             string+=str(i)
             string+=' '
         print(string)
